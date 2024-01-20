253496433ee737c7d8efd292704522534d1a9317
Felipe Moraes <felipemoraes.ti@outlook.com>
2024-01-20 11:10:48 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import { app } from './app'
+import { env } from './env'
+
+app.listen({
+    port: env.PORT,
+    host: '0.0.0.0',
+}).then((address) => {
+    console.log(`🚀 Server listening at ${address}`)
+})
